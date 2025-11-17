9ee123cac3c53de238376f891a0ba1125040ec57
Arthur-MARKOWICZ <arthurlopes25072005@gmail.com>
2025-11-17 15:13:27 -0300

    criando o comeco do menu

--- src/main/java/com/sistema/Main.java
+++ src/main/java/com/sistema/Main.java
@@ -1,25 +1,64 @@
 package com.sistema;
 
+import com.sistema.model.Mouse.MouseDAO;
+import com.sistema.model.Mousepad.Mousepad;
+import com.sistema.model.Mousepad.MousepadDAO;
+import com.sistema.model.Mousepad.MousepadService;
+import com.sistema.model.Notebook.NotebookDAO;
+import com.sistema.model.Notebook.NotebookService;
+import com.sistema.model.Teclado.TecladoDAO;
+import com.sistema.model.Teclado.TecladoService;
 import com.sistema.model.Usuario.*;
 
 import javax.persistence.*;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         UsuarioDAO usuarioDAO = new UsuarioDAO();
-       UsuarioService usuarioService = new UsuarioService(usuarioDAO);
-       UsuarioDto usuarioDto = new UsuarioDto("test","test10uu-9u9-@gmail.com","test");
-       var usuario = usuarioService.criar(usuarioDto);
-        System.out.println(usuario.getId());
-        System.out.println(usuario.getNome());
-        UsuarioDto usuarioAtualizar = new UsuarioDto("test 2","test@gmail.com","test");
-        var usuarioAtualizado = usuarioService.editar(usuario.getId(),usuarioAtualizar);
-        System.out.println(usuarioAtualizado.getId());
-        System.out.println(usuarioAtualizado.getNome());
-        usuarioService.deletar(4l);
-        var usuariobuscado = usuarioService.buscar(2l);
-        System.out.println(usuariobuscado.getNome());
+        UsuarioService usuarioService = new UsuarioService(usuarioDAO);
+        NotebookDAO notebookDAO = new NotebookDAO();
+        NotebookService notebookService= new NotebookService(notebookDAO);
+        TecladoDAO tecladoDAO = new TecladoDAO();
+        TecladoService tecladoService = new TecladoService(tecladoDAO);
+        MousepadDAO mousepadDAO = new MousepadDAO();
+        MousepadService mousepadService = new MousepadService(mousepadDAO);
+        MouseDAO mouseDAO = new MouseDAO();
 
-
+        int opcao = 0 ;
+        while (opcao == -1){
+            switch (opcao){
+                case 0:
+                    System.out.println("1-criar usuario");
+                    System.out.println("2-buscar usuario");
+                    System.out.println("3-editar usuario");
+                    System.out.println("4-deletar usuario");
+                    System.out.println("5-criar notebook");
+                    System.out.println("6-buscar notebook");
+                    System.out.println("7-editar notebook");
+                    System.out.println("8-deletar notebook");
+                    System.out.println("9-criar teclado");
+                    System.out.println("10-buscar teclado");
+                    System.out.println("11-editar teclado");
+                    System.out.println("12-deletar teclado");
+                    System.out.println("13-criar Mousepad ");
+                    System.out.println("14-editar Mousepad ");
+                    System.out.println("15-buscar Mousepad ");
+                    System.out.println("16-deletar Mousepad ");
+                    System.out.println("17-criar Mouse ");
+                    System.out.println("18-editar Mouse ");
+                    System.out.println("19-buscar Mouse ");
+                    System.out.println("20-deletar Mouse ");
+                    System.out.println("21-criar Fone ");
+                    System.out.println("22-buscar Fone ");
+                    System.out.println("23-editar Fone ");
+                    System.out.println("24-deletar Fone ");
+                    System.out.println("25-criar Cadeira ");
+                    System.out.println("26-buscar Cadeira ");
+                    System.out.println("27-deletar Cadeira ");
+                    System.out.println("28-editar Cadeira ");
+            }
+        }
     }
 }
