b79505c4d9b2eaf0a68cccb9672b9c7b83098800
Arthur-MARKOWICZ <arthurlopes25072005@gmail.com>
2025-11-17 14:37:45 -0300

    feat:colocando o test na main

--- src/main/java/com/sistema/Main.java
+++ src/main/java/com/sistema/Main.java
@@ -8,14 +8,18 @@ public class Main {
     public static void main(String[] args) {
         UsuarioDAO usuarioDAO = new UsuarioDAO();
        UsuarioService usuarioService = new UsuarioService(usuarioDAO);
-       UsuarioDto usuarioDto = new UsuarioDto("test","test1@test.com","test");
+       UsuarioDto usuarioDto = new UsuarioDto("test","test10uu-9u9-@gmail.com","test");
        var usuario = usuarioService.criar(usuarioDto);
         System.out.println(usuario.getId());
         System.out.println(usuario.getNome());
-        UsuarioDto usuarioAtualizar = new UsuarioDto("test 2","test@tes1`.com","test");
+        UsuarioDto usuarioAtualizar = new UsuarioDto("test 2","test@gmail.com","test");
         var usuarioAtualizado = usuarioService.editar(usuario.getId(),usuarioAtualizar);
         System.out.println(usuarioAtualizado.getId());
         System.out.println(usuarioAtualizado.getNome());
+        usuarioService.deletar(4l);
+        var usuariobuscado = usuarioService.buscar(2l);
+        System.out.println(usuariobuscado.getNome());
+
 
     }
 }
